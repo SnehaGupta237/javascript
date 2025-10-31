7f6438f4b9e02ce0a0717d6981094511653463d4
SnehaGupta237 <snehagupta224451@gmail.com>
2025-10-31 23:08:47 +0000

    variables

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2407086                        //change nahi kiya jaa sakta
+let accountEmail = "snhe@gmail.com"
+var accountPassword = "sne123"                        //do not use var, use let
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 23  //not allowed
+accountEmail = "sneha@gmail.com"
+accountPassword = "123"
+accountCity = "jaipur"
+console.log(accountId);
+console.table([ accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* this is a 
+multiline comment  */
