8fecc1d75c6470f86c91726abb1d025701c44ca7
SnehaGupta237 <snehagupta224451@gmail.com>
2025-11-09 09:25:58 +0000

    memory file added

---
+++ 1_basics/1.7_memory.js
@@ -0,0 +1,38 @@
+// primitive datatype (7)
+// string, number, boolean, null,undefined,bigint, symbol
+
+let name="sneha"
+
+const age=0 //variable create kiya h par uske value 0 hai
+
+let marritalStatus= false
+ 
+let inRelationship=null 
+ //variable create kiya h par uske value empty h
+let userEmail;
+ //variable create kiya h par uske value nahi diya h
+const speedOfLight=127474848439384747
+
+const id=Symbol('123')
+const id2=Symbol('123')         //both are different
+
+console.log( typeof marritalStatus)
+
+//Reference datatype(3)(non primitive)
+//array, object, function
+
+const fruits=["mango","apple","banana","cherry"]    //array
+
+let object={                         //object
+  name:"sneha",
+  Age:20,
+  female:true
+
+}
+
+ const myFunction=function(){        //function
+  console.log("hello world")
+}
+console.log(myFunction)
+
+// teeno primitive datatype ka type object hota h
