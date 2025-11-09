65bdc7909f927973967c726d31a9fdfd98643b92
SnehaGupta237 <snehagupta224451@gmail.com>
2025-11-09 09:25:00 +0000

    operation file added

---
+++ 1_basics/1.4_operations.js
@@ -0,0 +1,16 @@
+console.log(2+2)     
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)  //power 2^3
+console.log(2/2)
+console.log(2%2)
+
+console.log("1" + 2 + 3)       //123
+console.log(1 +2 + "3")       //33
+
+let gameCounter = 100;
+gameCounter++   
+console.log(gameCounter)    //101
+
+
+
