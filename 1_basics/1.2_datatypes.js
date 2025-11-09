caa2932ebb368bbd279b15838eb66aa364c31938
SnehaGupta237 <snehagupta224451@gmail.com>
2025-11-09 07:37:06 +0000

    rename file

---
+++ 1_basics/1.2_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  //treats all JS code as new version
+
+// alert("be alert")     //will not work coz v are using nodejs, not browser
+
+//type of datatypes
+// number
+// bigint  =>large number
+// boolean
+//string
+//null        =>empty space
+//undefined     =>vaiable declare kiya h par value nahi diya h
+//symbol =>unique
+
+
+
+console.log(typeof undefined); //undefined
+console.log(null); //object
