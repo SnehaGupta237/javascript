45c4f201df7d1438f7c0632cf513f5fc98105b78
SnehaGupta237 <snehagupta224451@gmail.com>
2025-11-09 09:25:25 +0000

    comparison file added

---
+++ 1_basics/1.5_comparison.js
@@ -0,0 +1,15 @@
+console.log(1>1)  //f
+console.log(1<1)//f
+console.log(1>=1)//t
+console.log(1<=1)//t                                           
+
+
+console.log(null>0) //f
+console.log(null==0) //f
+console.log(null>=0) //t
+
+
+//strict check ===
+console.log(2 === 2)  //t
+console.log("2" === 2)//f
+console.log(2 == 2)  // t ..double equal main datatype check nahi hota
